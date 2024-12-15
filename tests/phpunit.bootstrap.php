5858bfbe7492e5a313a389636da4ed51b7e6ff8d
kafoso <soefritz@gmail.com>
2024-12-15 14:58:59 +0100

    Require new bootstrap file
    Primarily for the constant "PROJECT_ROOT_DIRECTORY_PATH".

--- tests/phpunit.bootstrap.php
+++ tests/phpunit.bootstrap.php
@@ -1,7 +1,9 @@
 <?php
+// phpcs:ignoreFile
 
 declare(strict_types=1);
 
-require dirname(__DIR__) . '/vendor/autoload.php';
-
 define('TEST_ROOT_PATH', __DIR__);
+ini_set('memory_limit', '256M');
+
+require dirname(__DIR__) . '/script/bootstrap.php';
